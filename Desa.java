ca9586bc7bafc35e58d95b10c3ce3bd8feb435f8
Luiza <159278111+luizaqueiiroz@users.noreply.github.com>
2024-08-07 16:18:21 -0300

    primeiro commit

---
+++ Desa.java
@@ -0,0 +1,63 @@
+package desafio;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Desa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+			
+			
+			String [] listadeprodutos;
+			ArrayList<String> novaLista = new ArrayList<String>(Arrays.asList(produtos));
+			Scanner ler = new Scanner (System.in);
+			ArrayList<Integer> num = new ArrayList <>();
+			int valor = 0;
+			String produt;
+			int soma;
+			int i=0;
+			
+			novaLista.add("Lista de produtos");
+			
+
+			
+				for (String i1: novaLista)
+				{
+					System.out.println("listadeprodutos: "+ i1);
+
+			} while (i != 0);
+
+			for (int f = 1; f < 100; f++);
+			System.out.println("Digite"+ i +"valor");
+			String novaLista1=ler.nextLine();
+			novaLista1.add(produtos);
+
+				do {
+					System.out.println("Informe o valor dos produtos na ordem digitada");
+					i = ler.nextInt();
+					num.add(i);
+
+				} while (valor != 0);
+
+				for (valor = 0, valor < i) {
+					System.out.println("Os valores são:" + valor);
+				}  
+				System.out.println(i + valor);
+				if (soma)
+					soma =	valor + i;
+				System.out.println("O valor total é " + soma);
+				
+				
+
+			} 
+			System.out.println("valor"+ novaLista);
+		}
+
+	
+
+	
+
+
---
+++ Desafio1.java
@@ -0,0 +1,35 @@
+package desafio;
+
+public class Desafio1 {
+
+
+	import java.util.ArrayList;
+	import java.util.Scanner;
+
+	public static void main(String[]args) {
+		int valor = 0;
+		String produt;
+		int soma;
+		int i=0;
+		
+	}
+		for (produt=0,i < prod,i++) {
+			System.out.println("Os produtos são:" + i);
+
+			do {
+				System.out.println("Informe o valor dos produtos na ordem digitada");
+				i = ler.nextInt();
+				num.add(i);
+
+			} while (valor != 0);
+
+			for (produt) {
+				System.out.println("Os valores são:" +valor);
+			} 
+			System.out.println(i+valor);
+			if (soma)
+				soma =	valor + i;
+			System.out.println("O valor total é " +soma);
+
+		} 
+	}
